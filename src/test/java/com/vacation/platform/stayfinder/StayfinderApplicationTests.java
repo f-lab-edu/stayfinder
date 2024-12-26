2b1febcc9ea6f16968bcff7c390ccd644d13ff1f
안승현 <ansh@naunbiz.co.kr>
2024-12-26 22:33:38 +0900

    환경 설정

---
+++ src/test/java/com/vacation/platform/stayfinder/StayfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacation.platform.stayfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

28d7fc2d26d4bc51b2f50e5a396e2377d1329aa4
안승현 <ansh@naunbiz.co.kr>
2025-01-02 22:51:46 +0900

    user controller, service, repository 생성

---
+++ src/main/java/com/vacation/platform/stayfinder/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.vacation.platform.stayfinder.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/user")
+@RestController
+public class UserController {
+
+
+
+}

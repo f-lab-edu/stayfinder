a5325284d9a65a3c8848e078bc4487b2c46c4c31
안승현 <ansh@naunbiz.co.kr>
2025-01-05 21:47:02 +0900

    user 비즈니스 클래스 작성

--- src/main/java/com/vacation/platform/stayfinder/user/repository/UserRepository.java
+++ src/main/java/com/vacation/platform/stayfinder/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.vacation.platform.stayfinder.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
+    List<User> findByNickName(String nickName);
 }

826f0d9322db84ac42c1aeb6cc5316f7b70b07a1
안승현 <ansh@naunbiz.co.kr>
2025-01-11 22:08:26 +0900

    sms 전송 로직 수정

--- src/main/java/com/vacation/platform/stayfinder/certify/controller/CertifyController.java
+++ src/main/java/com/vacation/platform/stayfinder/certify/controller/CertifyController.java
@@ -4,9 +4,6 @@ import com.vacation.platform.stayfinder.certify.service.CertifyService;
 import com.vacation.platform.stayfinder.certify.service.serviceImpl.CertifyServiceImpl;
 import com.vacation.platform.stayfinder.util.Result;
 import jakarta.validation.Valid;
-import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.service.DefaultMessageService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,22 +15,10 @@ public class CertifyController {
 
     // 휴대폰 인증 요청 하고 응답 받으면
     // 유저 회원 컨트롤러로 넘긴다.
-    private final DefaultMessageService defaultMessageService;
-
-
-    @Value("${aes256.key}")
-    private String key;
-
-    @Value("${aes256.iv}")
-    private String iv;
 
     private final CertifyService certifyService;
 
-    public CertifyController(@Value("${nurigo.app.apikey}") String apiKey,
-                             @Value("${nurigo.app.secret_key}") String secretKey,
-                             @Value("${nurigo.app.domain}") String domain,
-                             CertifyServiceImpl certifyService) {
-        this.defaultMessageService = NurigoApp.INSTANCE.initialize(apiKey, secretKey, domain);
+    public CertifyController(CertifyServiceImpl certifyService) {
         this.certifyService = certifyService;
     }
 

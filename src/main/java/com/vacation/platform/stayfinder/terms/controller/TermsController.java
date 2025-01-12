5819c5f65edbbbf25df7817e92ab7697d4082173
안승현 <ansh@naunbiz.co.kr>
2025-01-12 21:35:50 +0900

    약관 수정 저장

--- src/main/java/com/vacation/platform/stayfinder/terms/controller/TermsController.java
+++ src/main/java/com/vacation/platform/stayfinder/terms/controller/TermsController.java
@@ -6,6 +6,7 @@ import com.vacation.platform.stayfinder.terms.entity.TermsSub;
 import com.vacation.platform.stayfinder.terms.service.TermsService;
 import com.vacation.platform.stayfinder.terms.service.serviceImpl.TermsServiceImpl;
 import com.vacation.platform.stayfinder.util.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class TermsController {
     //처음에 약관 동의 하는 api 전송
     //특정 고객이 약관 상세 보고 싶다면 해당 약관 sub 전송
     //메인 동의 api 에서 응답으로 필수 동의를 전체 동의 한 경우에 휴대폰 인증으로 넘어감
-    @PostMapping("/main")
+    @GetMapping("/main")
     public Result<Terms> getMain() {
         return termsService.getTermsMain();
     }
--- src/main/java/com/vacation/platform/stayfinder/terms/service/TermsService.java
+++ src/main/java/com/vacation/platform/stayfinder/terms/service/TermsService.java
@@ -2,14 +2,17 @@ package com.vacation.platform.stayfinder.terms.service;
 
 import com.vacation.platform.stayfinder.terms.dto.TermsDto;
 import com.vacation.platform.stayfinder.terms.entity.Terms;
+import com.vacation.platform.stayfinder.terms.entity.TermsSub;
 import com.vacation.platform.stayfinder.util.Result;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface TermsService {
 
-    public Result<Terms> getTermsMain();
+    Result<Terms> getTermsMain();
 
-    public Result<?> registerTerms (TermsDto termsDto);
+    Result<TermsSub> getTermsSub();
+
+    Result<?> registerTerms (TermsDto termsDto);
 
 }

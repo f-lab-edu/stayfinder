0c7837acb748a16d7d8bf00d0e931e7e5c6c410f
안승현 <abeb3@naver.com>
2025-01-22 15:23:44 +0900

    약관 dto 주석 등록

--- src/main/java/com/vacation/platform/stayfinder/terms/dto/TermsDto.java
+++ src/main/java/com/vacation/platform/stayfinder/terms/dto/TermsDto.java
@@ -8,11 +8,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TermsDto {
-    private String mainTitle;
-    private String subTitle;
-    private String detailContent;
-    private Boolean isRequired;
-    private Boolean isCompulsion;  // 기존 약관이 존재한다면 강제 업데이트
-    private Long termsMainId;
-    private Long version;
+    private Long termsMainId;           /* 약관 메인 ID */
+    private String mainTitle;           /* 약관 메인 제목 */
+    private String subTitle;            /* 약관 상세 제목 */
+    private String detailContent;       /* 약관 상세 내용 */
+    private Boolean isRequired;         /* 약관 필수 여부 */
+    private Long version;               /* 약관 버전 */
+    private Boolean isCompulsion;       /* 기존 약관이 존재한다면 강제 업데이트 */
+    private Boolean isAgreement;         /* 약관 동의 여부 */
 }

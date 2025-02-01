5743aa9030d412745eca9c708b74565a52ecd3a9
안승현 <abeb3@naver.com>
2025-02-02 00:14:38 +0900

    jwt 유틸 생성

---
+++ src/main/java/com/vacation/platform/stayfinder/util/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.vacation.platform.stayfinder.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("")
+    private String secretKey;
+
+    public String generateToken(String userName) {
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String userName) {
+        String tokenUserName = extractUserName(token);
+        return (userName.equals(tokenUserName) && !isTokenExpired(token));
+    }
+
+
+    public String extractUserName(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+
+}

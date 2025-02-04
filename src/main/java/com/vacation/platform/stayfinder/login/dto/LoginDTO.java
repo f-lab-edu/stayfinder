3cd65d60a69289549e63068280cea59c515e25e7
안승현 <abeb3@naver.com>
2025-02-04 16:49:23 +0900

    LoginDTO vaild 추가

--- src/main/java/com/vacation/platform/stayfinder/login/dto/LoginDTO.java
+++ src/main/java/com/vacation/platform/stayfinder/login/dto/LoginDTO.java
@@ -1,11 +1,16 @@
 package com.vacation.platform.stayfinder.login.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
+    @NotBlank(message = "아이디는 필수 값입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
     private String password;
+
     private String accessToken;
     private String refreshToken;
 }

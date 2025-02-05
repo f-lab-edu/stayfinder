9d78e276c03c0582342060a377d8e940d1711e89
안승현 <abeb3@naver.com>
2025-02-05 22:03:43 +0900

    exception @valid 어노테이션 처리 추가

--- src/main/java/com/vacation/platform/stayfinder/common/StayFinderExceptionHandler.java
+++ src/main/java/com/vacation/platform/stayfinder/common/StayFinderExceptionHandler.java
@@ -5,8 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -23,10 +27,28 @@ public class StayFinderExceptionHandler {
     }
 
     @Order(2)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StayFinderResponseDTO<?>> handleValidationException(MethodArgumentNotValidException e) {
+        List<String> errorMessages = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage).toList();
+
+        log.info("MethodArgumentNotValidException {}", errorMessages.get(0));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new StayFinderResponseDTO<>(
+                        ErrorType.VALID_ERROR.getCode(),
+                        errorMessages.get(0), // 첫 번째 에러 메시지만 응답
+                        null
+                ));
+    }
+
+    @Order(3)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<StayFinderResponseDTO<?>> handleGenericException(Exception e) {
 
-        log.error("Exception {}", (Object) e.getStackTrace());
+        log.error("Exception StackTrace {}", (Object) e.getStackTrace());
+        log.error("Exception Message {}", e.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new StayFinderResponseDTO<>(

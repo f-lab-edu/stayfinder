7689330dfe8bb9e95416e3a0d2224c48c22d0c26
안승현 <abeb3@naver.com>
2025-02-08 17:29:58 +0900

    CertifyRequestDto valid 값 처리

--- src/main/java/com/vacation/platform/stayfinder/certify/dto/CertifyRequestDto.java
+++ src/main/java/com/vacation/platform/stayfinder/certify/dto/CertifyRequestDto.java
@@ -1,6 +1,7 @@
 package com.vacation.platform.stayfinder.certify.dto;
 
 import com.vacation.platform.stayfinder.terms.dto.TermsDto;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CertifyRequestDto {
+    @NotBlank(message = "핸드폰 번호는 필수 입니다.")
     private String phoneNumber;             /* 핸드폰 번호 */
+    @NotBlank(message = "시스템 에러 입니다.")
     private List<TermsDto> termsDtoList;    /* 약관 동의 리스트 */
-    private Integer reqCertifyNumber;        /* 약관 인증 번호 */
+    @NotBlank(message = "핸드폰 인증번호는 필수 입니다.")
+    private Integer reqCertifyNumber;        /* 핸드폰 인증 번호 */
+    @NotBlank(message = "시스템 에러 입니다.")
     private Integer tryNumber;               /* 인증 요청 횟수 */
+    @NotBlank(message = "시스템 에러 입니다.")
     private Boolean isCertify;              /* 인증 완료 여부 */
 }

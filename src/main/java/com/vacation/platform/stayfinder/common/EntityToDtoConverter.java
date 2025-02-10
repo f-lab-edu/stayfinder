d1e864299a8272dda3ed2a40dc97b111a303cef0
안승현 <abeb3@naver.com>
2025-02-10 16:18:58 +0900

    메인 약관 조회 response 수정

--- src/main/java/com/vacation/platform/stayfinder/common/EntityToDtoConverter.java
+++ src/main/java/com/vacation/platform/stayfinder/common/EntityToDtoConverter.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class EntityToDtoConverter {
 
     private static final ObjectMapper mapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 👈 알 수 없는 필드 무시
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     public static <E, D> D convertEntityToDto(E entity, Class<D> dtoClass) {
         if(entity != null) {

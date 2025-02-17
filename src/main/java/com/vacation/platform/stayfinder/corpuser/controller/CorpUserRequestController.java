27d6b87d1c84133012ab260dcc6da1393ab1c1d0
안승현 <abeb3@naver.com>
2025-02-17 14:05:59 +0900

    기업회원 가입 요청 로직 완성

--- src/main/java/com/vacation/platform/stayfinder/corpuser/controller/CorpUserRequestController.java
+++ src/main/java/com/vacation/platform/stayfinder/corpuser/controller/CorpUserRequestController.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/api/v1/corp/request")
+@RestController
+@RequestMapping("/api/v1/corp/request")
 @RequiredArgsConstructor
 public class CorpUserRequestController {
 

dc162efa8188844e3318ed4de1bba324d41bc588
안승현 <abeb3@naver.com>
2025-03-22 17:42:19 +0900

    JpaRepository 상속 변경

--- corp/src/main/java/com/vacation/platform/corp/corperation/repository/CorporationRepository.java
+++ corp/src/main/java/com/vacation/platform/corp/corperation/repository/CorporationRepository.java
@@ -3,13 +3,13 @@ package com.vacation.platform.corp.corperation.repository;
 import com.vacation.platform.corp.corperation.entity.CorpStatus;
 import com.vacation.platform.corp.corperation.entity.Corporation;
 import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 @Registered
-public interface CorporationRepository extends CrudRepository<Corporation,String> {
+public interface CorporationRepository extends JpaRepository<Corporation,String> {
 
 	@Query("SELECT c FROM Corporation c WHERE c.businessLicense = :businessLicense AND c.corpStatus = :corpStatus")
 	Optional<Corporation> findByBusinessLicense(String businessLicense, CorpStatus corpStatus);
